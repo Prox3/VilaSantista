fca2ba7fff120a8156c170c9746c061df19e7cb0
Kevin Lira <lira.kevin@prox3.com.br>
2013-03-16 17:26:40 -0300

    modificacao no footer

--- wordpress/wp-content/themes/theme_vila_santista/footer.php
+++ wordpress/wp-content/themes/theme_vila_santista/footer.php
@@ -1,22 +1,22 @@
-	<div id="footer">
-			<?php wp_footer(); ?>
+	<div id="footer" class="left">
+			
             <div class="footer-top fundo">
-                <div class="footer-top-texto">Home</div>
+                <div class="footer-top-texto"><?php wp_nav_menu( "MenuFooter" ); ?></div>
                 <div class="footer-top-telefone">4646-4646 / 6464/6464</div>
             </div>
             <div class="footer-bottom fundo">
                 <div class="footer-endereco-texto">
-                    Av. Edson Consolmagno, 150 - Ponte Grnade - Mogi das Cruzes/SP    
+                    Av. Dr. Edison Consolmagno, 150 – Ponte Grande – Mogi das Cruzes/SP    
                     Vila Santista © Todos direitos reservados - 2013
                 </div>
                 <div class="footer-act-logo">
-                    <img src="images/actmob-footer-logo.png" />
+                    
                 </div>
             </div>
             <div class="footer-topo-botao">
             </div>
         </div>
-	
+	<?php wp_footer(); ?>
 </div>
 	
 </body>
