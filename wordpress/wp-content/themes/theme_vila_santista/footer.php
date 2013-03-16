0799abce0a9c6d0288ea7782eb9ef9e73183cca4
Kevin Lira <lira.kevin@prox3.com.br>
2013-03-16 15:56:34 -0300

    Modificação no footer e adicao do style2.css (css do footer)

--- wordpress/wp-content/themes/theme_vila_santista/footer.php
+++ wordpress/wp-content/themes/theme_vila_santista/footer.php
@@ -1,7 +1,21 @@
 	<div id="footer">
-		<p>&copy; 2012 - Todos os Direitos Reservados</p>
-		<?php wp_footer(); ?>
-	</div>
+			<?php wp_footer(); ?>
+            <div class="footer-top fundo">
+                <div class="footer-top-texto">Home</div>
+                <div class="footer-top-telefone">4646-4646 / 6464/6464</div>
+            </div>
+            <div class="footer-bottom fundo">
+                <div class="footer-endereco-texto">
+                    Av. Edson Consolmagno, 150 - Ponte Grnade - Mogi das Cruzes/SP    
+                    Vila Santista © Todos direitos reservados - 2013
+                </div>
+                <div class="footer-act-logo">
+                    <img src="images/actmob-footer-logo.png" />
+                </div>
+            </div>
+            <div class="footer-topo-botao">
+            </div>
+        </div>
 	
 </div>
 	
