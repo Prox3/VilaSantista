8e51b954f9bcd9a4c45f432e7c98ecb4f94f10db
DennerLuan <denner.luan@prox3.com.br>
2013-03-16 13:45:17 -0300

    Modificações na header

--- wordpress/wp-content/themes/theme_vila_santista/header.php
+++ wordpress/wp-content/themes/theme_vila_santista/header.php
@@ -5,18 +5,29 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <title><?php wp_title(''); ?></title>
+<link rel="stylesheet" type="text/css" href="<?= bloginfo( 'template_url' ) ?>/css/print.css" media="print" />
+<!--[if IE]> <link href="<?= bloginfo( 'template_url' ) ?>/css/ie.css" media="screen" rel="stylesheet" type="text/css" ><![endif]-->
+<link href="<?= bloginfo( 'template_url' ) ?>/css/screen.css" media="screen" rel="stylesheet" type="text/css" >
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<script src="<?= bloginfo( 'template_url' ) ?>/js/jquery.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+    $(".dropdown-horizontal-container").css("background-color", "none");
+  });
+</script>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); wp_head(); ?>
 
 </head>
 <body>
-<div id="corpo">
-	<div id="header">
-		<h1><?php bloginfo('name'); ?></h1>
-		<h2><?php bloginfo('description'); ?></h2>
-		
-		<ul id="nav">
-			<?php wp_list_cats('sort_column=name'); ?>
-		</ul>
+<div class="container">
+	<div id="header" class="left">
+  	<div id="bg_header"></div>
+  	<div id="bg_nuvens">
+    	<a class="logo left" target="_self" href="">Logo Vila Santista</a>
+      <div class="body_navigator right">
+    		<?php dynamic_sidebar( 'sidebarMenu' ); ?>
+    	</div>
+      <div class="body_slogan right"></div>
+    </div>
 	</div>
